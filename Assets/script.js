7b5a7b0eafbc3d4c3c888f02bcbc4ec024d2c438
Rudy Sanchez <rudys212@gmail.com>
2021-04-15 02:18:58 -0700

    updated HTML

---
+++ Assets/script.js
@@ -0,0 +1,21 @@
+//var for questions is needed. can set up in an array. how many questions??
+//var for answer to questions
+//var for choices to questions
+//var for timer. how much time???
+//var for penalized time
+//var for score
+var index=0 
+// variables being set for questions
+var qstns=[
+    {
+        question:"Question 1: How many HTML heading levels exist?",
+        choices:["a. 9", "b. 10", "c. 7", "d. 8"],
+        answer: "c. 7"
+    },
+    {
+        question:"Question 2: How many expressions makeup a for loop?",
+    }
+]
+
+
+ 
